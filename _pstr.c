b31955122579cd038a8c092fdc395df1d271cfb5
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-01-20 15:53:19 +0000

    fix

--- _pstr.c
+++ _pstr.c
@@ -8,16 +8,17 @@
  */
 void _pstr(stack_t **stack, unsigned int line_num)
 {
+	stack_t *head = *stack;
 	(void)line_num;
 	while (1)
 	{
-		if (!(*stack) || (*stack)->n <= 0 || (*stack)->n > 127)
+		if (!(*stack) || (*stack)->n <= 0 || (*stack)->n > 127 || *stack == NULL)
 		{
 			printf("\n");
 			break;
 		}
 		printf("%c", (*stack)->n);
 		*stack = (*stack)->next;
-		/*_pop(&head, line_num);*/
+		_pop(&head, line_num);
 	}
 }
